a42e4c2556bdbd32c8e62fe193b110e8bc67153b
Krishna Kumar <93042057+krishnakumar1204@users.noreply.github.com>
2023-07-13 09:56:35 +0530

    Disabled the buttons for empty text box

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -29,21 +29,21 @@ const TextForm = (props) => {
     const handleUpClick = () => {
         const newText = text.toUpperCase();
         setText(newText);
-        text.length>0?props.showAlert("Converted to Uppercase!", "success"):props.showAlert("Enter Your Text to convert to Uppercase", "warning");
+        props.showAlert("Converted to Uppercase!", "success");
     }
 
     // To Convert Lowercase
     const handleLowClick = () => {
         const newText = text.toLowerCase();
         setText(newText);
-        text.length>0?props.showAlert("Converted to Lowercase!", "success"):props.showAlert("Enter Your Text to convert to Uppercase", "warning");
+        props.showAlert("Converted to Lowercase!", "success");
     }
 
     // To Remove Extra Spaces
     const handleExtraSpaces =()=>{
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
-        text.length>0?props.showAlert("Extra spaces have been Removed!", "success"):props.showAlert("Enter Your Text First", "warning");
+        props.showAlert("Extra spaces have been Removed!", "success");
     }
 
     // For Text-To-Speech
@@ -51,20 +51,20 @@ const TextForm = (props) => {
         let msg = new SpeechSynthesisUtterance();
         msg.text = text;
         window.speechSynthesis.speak(msg);
-        text.length>0?props.showAlert("Speech Started!", "success"):props.showAlert("Enter Your Text First", "warning");
+        props.showAlert("Speech Started!", "success");
     }
 
     // To Clear All Text
     const handleClearClick = () => {
         const newText = "";
         setText(newText);
-        text.length>0?props.showAlert("Deleted All Text", "warning"):props.showAlert("Enter Your Text First", "warning");
+        props.showAlert("Deleted All Text", "warning");
     }
 
     // To Copy the text to clipboard
     const copyText = () => {
         navigator.clipboard.writeText(text);
-        text.length>0?props.showAlert("Copied To clipboard!", "success"):props.showAlert("Enter Your Text First", "warning");
+        props.showAlert("Copied To clipboard!", "success");
     }
 
 
@@ -81,19 +81,19 @@ const TextForm = (props) => {
                 <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==="dark"?"#29465B":"white", color: props.mode==="dark"?"white":"black"}} id="myBox" rows="8"></textarea>
             </div>
             <input type="file" className="btn btn-secondary mx-1 btn-sm" accept="text/plain" onChange = {readTxt}/>
-            <button className="btn btn-primary mx-1 my-1 btn-sm" onClick={handleUpClick} >Convert to Uppercase</button>
-            <button className="btn btn-primary mx-1 my-1 btn-sm" onClick={handleLowClick} >Convert to Lowercase</button>
-            <button className="btn btn-primary mx-1 my-1 btn-sm" onClick={handleExtraSpaces} >Remove Extra Spaces</button>
-            <button className="btn btn-warning mx-1 my-1 btn-sm" type="submit" onClick={speak} >Speak</button>
-            <button className="btn btn-danger mx-1 my-1 btn-sm" onClick={handleClearClick} >Clear Text</button>
-            <button className="btn btn-primary mx-1 my-1 btn-sm" onClick={copyText} >Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1 btn-sm" onClick={handleUpClick} >Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1 btn-sm" onClick={handleLowClick} >Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1 btn-sm" onClick={handleExtraSpaces} >Remove Extra Spaces</button>
+            <button disabled={text.length===0} className="btn btn-warning mx-1 my-1 btn-sm" type="submit" onClick={speak} >Speak</button>
+            <button disabled={text.length===0} className="btn btn-danger mx-1 my-1 btn-sm" onClick={handleClearClick} >Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1 btn-sm" onClick={copyText} >Copy Text</button>
         </div>
         <div className="container my-3" style={{color: props.mode==="dark"?"white":"black"}}>
             <h2>Your Text Summary</h2>
             <p><b>{countWords(text)}</b> Words and <b>{text.length}</b> Characters. </p>
             <p><b>{(0.008 * text.split(" ").length).toFixed(2)}</b> Minutes Read</p>
             <h3>Preview</h3>
-            <p>{text.length>0?text:"Your Text Preview will appear here!"}</p>
+            <p>{text.length>0?text:"Enter Your in the box to see Preview Here"}</p>
         </div>
         </>
     )
